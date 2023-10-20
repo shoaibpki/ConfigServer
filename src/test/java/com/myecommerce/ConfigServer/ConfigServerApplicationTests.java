0f88cb12e3d4f6243c317be3aac1cb234fe8bf75
Shoaib Raza <89098029+shoaibpki@users.noreply.github.com>
2023-10-20 15:14:29 +0100

    upload inial version

---
+++ src/test/java/com/myecommerce/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.myecommerce.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootTest
+@EnableConfigServer
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
